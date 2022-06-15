642b0536804947c476d6ae43d17cb25cd39d0473
Graham Morby <43313226+GrahamMorbyDev@users.noreply.github.com>
2022-06-15 18:52:49 +0100

    updated URL for app

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "here goes your access token from BotFather"
 bot_user_name = "the username you entered"
-URL = "https://git.heroku.com/avatar-gram.git"
+URL = "https://avatar-gram.herokuapp.com/"
