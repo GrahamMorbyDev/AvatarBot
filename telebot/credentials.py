c5c09c46264c90fcc5a606f1e13b6ce0e9fda405
Graham Morby <43313226+GrahamMorbyDev@users.noreply.github.com>
2022-06-15 18:58:55 +0100

    updated creds

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "here goes your access token from BotFather"
-bot_user_name = "the username you entered"
+bot_token = "5589369261:AAHVmL7f0mMtuKVYHAFRLpbV9P1X4qQTQZM"
+bot_user_name = "avatargram"
 URL = "https://avatar-gram.herokuapp.com/"
